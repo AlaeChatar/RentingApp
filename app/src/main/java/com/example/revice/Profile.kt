c35d9c46c9b57a75704b8d455abab4f2ff159d22
Kiri <keanusegers@hotmail.com>
2024-11-13 16:58:13 +0100

    add dependencies firestore

--- app/src/main/java/com/example/revice/Profile.kt
+++ app/src/main/java/com/example/revice/Profile.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.revice.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Profile : AppCompatActivity() {
+
+    private lateinit var profileBinding: ActivityProfileBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+
+        profileBinding = ActivityProfileBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
+        setContentView(profileBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }

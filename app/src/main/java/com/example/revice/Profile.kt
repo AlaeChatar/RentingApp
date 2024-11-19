b4a7ab1ded5aebea9c85895845acf7c3db6b11ec
Kiri <keanusegers@hotmail.com>
2024-11-19 20:07:01 +0100

    Add update email

--- app/src/main/java/com/example/revice/Profile.kt
+++ app/src/main/java/com/example/revice/Profile.kt
@@ -2,22 +2,28 @@ package com.example.revice
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.revice.databinding.ActivityProfileBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Profile : AppCompatActivity() {
 
     private lateinit var profileBinding: ActivityProfileBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
 
         profileBinding = ActivityProfileBinding.inflate(layoutInflater)
 
@@ -31,6 +37,7 @@ class Profile : AppCompatActivity() {
 
         val btnDevices = profileBinding.btnDevices
         val etEmail = profileBinding.etEmailDB
+        val btnChangeEmail = profileBinding.btnChangeEmail
 
         etEmail.setText("${auth.currentUser?.email}")
 
@@ -38,6 +45,36 @@ class Profile : AppCompatActivity() {
             auth.signOut()
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        btnChangeEmail.setOnClickListener{
+            auth.currentUser?.verifyBeforeUpdateEmail(etEmail.text.toString())?.addOnCompleteListener{ task ->
+                if (task.isSuccessful){
+                    db.collection("users")
+                        .document(auth.currentUser!!.uid)
+                        .update("email", etEmail.text.toString())
+
+                    Toast.makeText(
+                        baseContext, "Email updated succesfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                    Toast.makeText(
+                        baseContext, "Email update failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (task.exception is FirebaseAuthUserCollisionException){
+                    Toast.makeText(
+                        baseContext, "Email update failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        baseContext, "Email update failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
 }
